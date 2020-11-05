646cd36781543ac1fe2e1caceb9302d83d93091c
GregoireEpsi <gregoire.barre@epsi.fr>
2020-11-05 08:30:07 +0100

    avant relier

--- index.php
+++ index.php
@@ -1,104 +1,107 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         <link rel="stylesheet" href="./css/base.css" type="text/css" media="screen">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/base.css" type="text/css" media="screen">
 </head>
+
 <body>
 
-<div class="header">
-  <div class="menuBurger">
-    <a href="#"><img src="./Images/Illustrations/SVG/Menu.svg"/></a>
-  </div>
+  <div class="header">
+    <div class="menuBurger">
+      <a href="#"><img src="./Images/Illustrations/SVG/Menu.svg" /></a>
+    </div>
 
-  <div class="titre">
-   localize
-  </div>
+    <div class="titre">
+      localize
+    </div>
 
-   <div class="description">
-     Innovons ensemble pour demain
-   </div>
+    <div class="description">
+      Innovons ensemble pour demain
+    </div>
 
-   <div class="buttonInscription">
-     Inscription
-   </div>
+    <div class="buttonInscription">
+      Inscription
+    </div>
 
-   <div class="buttonConnexion">
-     Connexion
-   </div>
+    <div class="buttonConnexion">
+      Connexion
+    </div>
 
-   <div class="buttonSavoirPlus">
-     <a href="#"><img src="./Images/Illustrations/SVG/Down.svg"/></a>
-     <br>
-     En savoir plus
-   </div>
-</div>
-
-<div class="row">
-  <div class="col-s-12 ">
-    <div class="divTitre">
-      Qui sommes nous?
+    <div class="buttonSavoirPlus">
+      <a href="#"><img src="./Images/Illustrations/SVG/Down.svg" /></a>
+      <br>
+      En savoir plus
     </div>
   </div>
 
-  <div class="col-s-6">
-    <div class="div1Section1">
-      <div class="div1TitreSection1">
-        Vos idées, notre priorité.
-      </div>
-      <div class="div1TextSection1">
-        <table>
-          <tr>
-            <td>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </td>
-            <td>
-              <a href="#"><img src="./Images/Illustrations/SVG/361_factory_worker_flatline.svg"/></a>
-            </td>
-          </tr>
-        </table>
-      </div>
+  <div class="row">
+    <div class="col-s-12 ">
+      <div class="divTitre">
+        Qui sommes nous?
       </div>
     </div>
 
-  <div class="col-s-6">
-    <div class="div2Section1">
-      <div class="div2TitreSection1">
-        Vos idées, notre priorité.
-      </div>
-      <div class="div2TextSection1">
-        <table>
-          <tr>
-            <td>
-              <a href="#"><img src="./Images/Illustrations/SVG/337_thinking_flatline.svg"/></a>
-            </td>
-            <td>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </td>
-          </tr>
-        </table>
+    <div class="col-s-6">
+      <div class="div1Section1">
+        <div class="div1TitreSection1">
+          Vos idées, notre priorité.
+        </div>
+        <div class="div1TextSection1">
+          <table>
+            <tr>
+              <td>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </td>
+              <td>
+                <a href="#"><img src="./Images/Illustrations/SVG/361_factory_worker_flatline.svg" /></a>
+              </td>
+            </tr>
+          </table>
+        </div>
       </div>
     </div>
-  </div>
-</div>
 
-<div class="row">
-  <div class="col-s-12 ">
-    <div class="divTitre">
-      Où nous utiliser?
-    </div>
-    <div class="div1Section2">
-      test
+    <div class="col-s-6">
+      <div class="div2Section1">
+        <div class="div2TitreSection1">
+          Vos idées, notre priorité.
+        </div>
+        <div class="div2TextSection1">
+          <table>
+            <tr>
+              <td>
+                <a href="#"><img src="./Images/Illustrations/SVG/337_thinking_flatline.svg" /></a>
+              </td>
+              <td>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
     </div>
   </div>
 
+  <div class="row">
+    <div class="col-s-12 ">
+      <div class="divTitre">
+        Où nous utiliser?
+      </div>
+      <div class="div1Section2">
+        test
+      </div>
+    </div>
 
 
-</div>
 
-<div class="footer">
-  <p>Resize the browser window to see how the content respond to the resizing.</p>
-</div>
+  </div>
+
+  <div class="footer">
+    <p>Resize the browser window to see how the content respond to the resizing.</p>
+  </div>
 
 </body>
+
 </html>
---
+++ inscription.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./css/inscription.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="retour">
+        <a href="#"><img src="./Images/Illustrations/SVG/Back.svg" /></a>
+    </div>
+    <div class="titre">
+        inscription
+    </div>
+    <div id="ecran">
+
+        <div class="formulaire">
+            <form>
+
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Prenom</label>
+                    <input id="inputPrenom " type="text" class="form-control">
+                </div>
+                <div class="form-group" id="">
+                    <label for="exampleInputPassword1">Nom</label>
+                    <input type="text" class="form-control" id="">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Adresse mail</label>
+                    <input type="email" class="form-control" id="">
+
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Mot de passe</label>
+                    <input type="password" class="form-control" id="">
+                </div>
+                <div id="bouttonDemarrer">
+                    Demarrer
+                </div>
+                <br>
+                <div class="lien">
+                    <a id="lienPropre" href="/connexion.php">J'ai déja un compte</a>
+                </div>
+                <br>
+                <p id="p">En cliquant sur le bouton "Démarrer", vous créez un compte localize, et acceptez les conditions d'utilisation et la politique de confidentialité de localize</p>
+            </form>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+
+</html>
