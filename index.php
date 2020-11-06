4628a7f2ac3efadf330bbf150aeb195d3d218bbf
GregoireEpsi <gregoire.barre@epsi.fr>
2020-11-06 11:16:21 +0100

    commit final

--- index.php
+++ index.php
@@ -54,17 +54,18 @@
 
     <div class="col-s-6">
       <div class="div1Section1">
-        <div class="div1TitreSection1">
+        <div class="div2TitreSection1">
           Vos idées, notre priorité.
         </div>
         <div class="div1TextSection1">
           <table>
             <tr>
               <td>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Localize vous permet d’exprimer vos besoins sous forme d’initiatives aux pouvoirs publics.
+                Besoin d’un nouvel arrêt de tram, d’un jardin participatif ou d’une déchèterie ? Osez proposer, ils vous écoutent.
               </td>
               <td>
-                <a href="#"><img src="./Images/Illustrations/SVG/361_factory_worker_flatline.svg" /></a>
+                <a href="#"><img src="./Images/Illustrations/SVG/337_thinking_flatline.svg " /></a>
               </td>
             </tr>
           </table>
@@ -75,16 +76,17 @@
     <div class="col-s-6">
       <div class="div2Section1">
         <div class="div2TitreSection1">
-          Vos idées, notre priorité.
+          Bâtir le futur, ensemble.
         </div>
         <div class="div2TextSection1">
           <table>
             <tr>
               <td>
-                <a href="#"><img src="./Images/Illustrations/SVG/337_thinking_flatline.svg" /></a>
+                <a href="#"><img src="./Images/Illustrations/SVG/361_factory_worker_flatline.svg" /></a>
               </td>
               <td>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Commentez, votez pour (ou contre) des initiatives près de chez vous.
+                Se réunir est un début, rester ensemble est un progrès, travailler ensemble est la réussite. Réinventons ensemble le monde de demain.
               </td>
             </tr>
           </table>
@@ -104,7 +106,7 @@
           <a href="#"><img src="./Images/Illustrations/SVG/Map.svg" /></a>
         </div>
         <div class="divTextSection">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Localize est une solution digitale déployée sur l’agglomération Nantaise depuis Novembre 2020.
         </div>
         <div class="buttonSection">
           Ajouter ma ville
@@ -123,7 +125,7 @@
           <a href="#"><img src="./Images/Illustrations/SVG/373_architect_flatline.svg" /></a>
         </div>
         <div class="divTextSection">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Chez Localize on ne vend pas que du rêve. Découvrez vos initiatives passées à l’étape de projet !
         </div>
         <div class="buttonSection">
           Découvrir
@@ -208,111 +210,112 @@
 
         </div>
       </div>
-
-      <div class="col-s-6">
-        <div class="divSection4">
-          <div class="divImageSection4">
-            <img src="./Images/Illustrations/SVG/287_startup_flatline.svg" />
-          </div>
-          <div class="divNomSection4">
-            Néhémie
-          </div>
-          <div class="divTexSection4">
-            Digital Marketing Manager
-          </div>
-        </div>
-      </div>
-
-      <div class="col-s-6">
-        <div class="divSection4">
-          <div class="divImageSection4">
-            <img src="./Images/Illustrations/SVG/230_designer_flatline.svg" />
-          </div>
-          <div class="divNomSection4">
-            Enzo
-          </div>
-          <div class="divTexSection4">
-            UX/UI Designer
-          </div>
-        </div>
-      </div>
-
-
-      <div class="col-s-6">
-        <div class="divbuttonSection4">
-          <div class="buttonSection">
-            Nous rejoindre
-          </div>
-        </div>
-      </div>
-
     </div>
 
-    <div class="row">
-      <div class="col-s-12">
-        <div id="5" class="titreSection">
-          Nous contacter
+    <div class="col-s-6">
+      <div class="divSection4">
+        <div class="divImageSection4">
+          <img src="./Images/Illustrations/SVG/287_startup_flatline.svg" />
         </div>
+        <div class="divNomSection4">
+          Néhémie
+        </div>
+        <div class="divTexSection4">
+          Digital Marketing Manager
+        </div>
+      </div>
+    </div>
 
-        <div class="divSection5">
-          <div class="divTextSection5">
-            Nom
-          </div>
-          <div class="divInputSection5">
-            <input type="text" placeholder="Entrez votre nom">
-          </div>
-          <div class="divTextSection5">
-            Adresse mail
-          </div>
-          <div class="divInputSection5">
-            <input type="text" placeholder="Entrez votre adresse mail">
-          </div>
-          <div class="divTextSection5">
-            Message
-          </div>
-          <div class="divInputSection5">
-            <textarea placeholder="Dites-nous tout..."></textarea>
-          </div>
-          <div class="buttonEnvoyer">
-            Envoyer
-          </div>
+    <div class="col-s-6">
+      <div class="divSection4">
+        <div class="divImageSection4">
+          <img src="./Images/Illustrations/SVG/230_designer_flatline.svg" />
+        </div>
+        <div class="divNomSection4">
+          Enzo
+        </div>
+        <div class="divTexSection4">
+          UX/UI Designer
         </div>
       </div>
     </div>
 
 
-    <div class="footer">
-      <div class="divTextfooter">
-        Mentions légales
-      </div>
-      <div class="divTextfooter">
-        Politique de confidentialité
-      </div>
-      <div class="divTextfooter">
-        Plan du site
-      </div>
-      <div class="divLogoFooter">
-        <table>
-          <tr>
-            <td>
-              <a href="https://www.facebook.com"><img src="./Images/Illustrations/SVG/Facebook.svg" />
-            </td>
-            <td>
-              <a href="https://www.linkedin.com/"><img src="./Images/Illustrations/SVG/LinkedIn.svg" />
-            </td>
-            <td>
-              <a href="https://www.twitter.com"><img src="./Images/Illustrations/SVG/Twitter.svg" />
-            </td>
-            <td>
-              <a href="https://www.youtube.com"><img src="./Images/Illustrations/SVG/YouTube.svg" />
-            </td>
-          </tr>
-        </table>
-      </div>
-      <div class="divTextfooter">
-        &copy; 2020 localize - Tous droits réservés
+    <div class="col-s-6">
+      <div class="divbuttonSection4">
+        <div class="buttonSection">
+          Nous rejoindre
+        </div>
       </div>
     </div>
+
+  </div>
+
+  <div class="row">
+    <div class="col-s-12">
+      <div id="5" class="titreSection">
+        Nous contacter
+      </div>
+
+      <div class="divSection5">
+        <div class="divTextSection5">
+          Nom
+        </div>
+        <div class="divInputSection5">
+          <input type="text" placeholder="Entrez votre nom">
+        </div>
+        <div class="divTextSection5">
+          Adresse mail
+        </div>
+        <div class="divInputSection5">
+          <input type="text" placeholder="Entrez votre adresse mail">
+        </div>
+        <div class="divTextSection5">
+          Message
+        </div>
+        <div class="divInputSection5">
+          <textarea placeholder="Dites-nous tout..."></textarea>
+        </div>
+        <div class="buttonEnvoyer">
+          Envoyer
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="footer">
+    <div class="divTextfooter">
+      Mentions légales
+    </div>
+    <div class="divTextfooter">
+      Politique de confidentialité
+    </div>
+    <div class="divTextfooter">
+      Plan du site
+    </div>
+    <div class="divLogoFooter">
+      <table>
+        <tr>
+          <td>
+            <a href="https://www.facebook.com"><img src="./Images/Illustrations/SVG/Facebook.svg" />
+          </td>
+          <td>
+            <a href="https://www.linkedin.com/"><img src="./Images/Illustrations/SVG/LinkedIn.svg" />
+          </td>
+          <td>
+            <a href="https://www.twitter.com"><img src="./Images/Illustrations/SVG/Twitter.svg" />
+          </td>
+          <td>
+            <a href="https://www.youtube.com"><img src="./Images/Illustrations/SVG/YouTube.svg" />
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div class="divTextfooter">
+      &copy; 2020 localize - Tous droits réservés
+    </div>
+  </div>
 
 
 </body>
